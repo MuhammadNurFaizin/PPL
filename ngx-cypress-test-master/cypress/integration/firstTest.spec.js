f5740f60215a076bbd9f7b50061197dbc811689d
MuhammadNurFaizin <muhammadnurfaizin611@gmial.com>
2020-10-22 23:58:19 +0700

    ppl

---
+++ ngx-cypress-test-master/cypress/integration/firstTest.spec.js
@@ -0,0 +1,67 @@
+/// <reference types="Cypress" />
+
+describe('My first test suite', () => {
+
+    beforeEach('visit url, open form and form layouts', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+    })
+
+    it('Inline form test', () => {
+        cy.get('.form-inline').within(() => {
+            cy.get('input[placeholder = "Jane Doe"]').type('Muhammad Nur Faizin').should('have.value', 'Muhammad Nur Faizin')
+            cy.get('input[placeholder = "Email"]').type('muhammadnurfaizin611@gmail.com').should('have.value', 'muhammadnurfaizin611@gmail.com')
+            cy.get('.custom-checkbox').click()
+        })
+    })
+
+    it('Using the grid test', () => {
+        cy.get('#inputEmail1').type('muhammadnurfaizin611@gmail.com').should('have.value', 'muhammadnurfaizin611@gmail.com')
+        cy.get('#inputPassword2').should('have.attr', 'type', 'password')
+        cy.get('#inputPassword2')
+            .type('113333')
+            .should('have.value', '113333')
+            .parents('form')
+            .find('.inner-circle')
+            .first()
+            .click()
+            .parents('form')
+            .contains('Option 2')
+            .parents('nb-radio')
+            .find('.inner-circle')
+            .click()
+            .parents('form')
+            .contains('Disabled Option')
+            .parents('nb-radio')
+            .should('have.attr', 'disabled')
+    });
+
+    it('Form without label test', () => {
+        cy.get('input[placeholder = "Recipients"]').type('tuantakur.uhuy@gmail.com').should('have.value', 'tuantakur.uhuy@gmail.com')
+        cy.get('input[placeholder = "Subject"]').type('Surat Lamaran Kerja').should('have.value', 'Surat Lamaran Kerja')
+        cy.get('textarea[placeholder = "Message"]').type('Selamat siang, Tuan Takur. Semoga surat ini bisa tembus.').should('contain.value', 'Selamat siang, Tuan Takur. Semoga surat ini bisa tembus.')
+    });
+
+    it('Basic form test', () => {
+        cy.get('#exampleInputEmail1')
+            .type('muhammadnurfaizin611@gmail.com')
+            .should('have.value', 'muhammadnurfaizin611@gmail.com')
+        cy.get('#exampleInputPassword1').should('have.attr', 'type', 'password')
+        cy.get('#exampleInputPassword1')
+            .type('113333')
+            .should('have.value', '113333')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+    });
+
+    it('Block form test', () => {
+        cy.get('#inputFirstName[placeholder = "First Name"]').type('Muhammad Nur').should('have.value', 'Muhammad Nur')
+        cy.get('#inputLastName[placeholder = "Last Name"]').type('Faizin').should('have.value', 'Faizin')
+        cy.get('#inputEmail[placeholder = "Email"]').type('muhammadnurfaizin611@gmail.com').should('have.value', 'muhammadnurfaizin611@gmail.com')
+        cy.get('#inputWebsite[placeholder = "Website"]').type('https://github.com/myusrilh').should('have.value', 'https://github.com/myusrilh')
+    });
+
+});
